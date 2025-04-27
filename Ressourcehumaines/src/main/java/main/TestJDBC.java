b84e1a38ada524b2ba8a0429b0fb8f09c54cc75a
maramgh24 <maram.ghribi@esprit.tn>
2025-04-27 14:26:00 +0100

    firstcommit

--- Ressourcehumaines/src/main/java/main/TestJDBC.java
+++ Ressourcehumaines/src/main/java/main/TestJDBC.java
@@ -1,21 +1,26 @@
 package main;
 
-import entities.Person;
+import entities.Annonce;
+import services.AnnonceService;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class TestJDBC {
 
     public static void main(String[] args) {
-        PersonService ps = new PersonService();
-        Person p = new Person(1,22, "Ali", "Ben Foulen");
-
+        AnnonceService annonceService = new AnnonceService();
+        Annonce annonce = new Annonce("Titre de l'annonce", "Contenu de l'annonce", LocalDateTime.now(), "piece_jointe.pdf");
 
         try {
-            System.out.println(ps.readAll());
+            // Pour insérer une nouvelle annonce dans la base de données
+            annonceService.create(annonce);
+            System.out.println("Annonce ajoutée avec succès");
+
+            // Lire toutes les annonces
+            System.out.println(annonceService.readAll());
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
-
     }
 }
