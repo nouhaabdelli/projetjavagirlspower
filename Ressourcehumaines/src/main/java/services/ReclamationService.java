9d8dac7c7325e007b062969e7890b5043d16e4f0
nouhaabdelli <nouha.abdelli@esprit.tn>
2025-04-27 17:30:10 +0100

    Create ReclamationService.java

---
+++ Ressourcehumaines/src/main/java/services/ReclamationService.java
@@ -0,0 +1,71 @@
+package services;
+
+import entities.Reclamations;
+import utils.MyConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+public   class ReclamationService implements IService<Reclamations> {
+
+    private Connection cnx;
+
+    public ReclamationService() {
+        cnx = MyConnection.getInstance().getConnection();
+    }
+
+    @Override
+    public void create(Reclamations reclamation) throws SQLException {
+        String query = "INSERT INTO reclamations(titre, description, date_creation, status) " +
+                "VALUES (?, ?, ?, ?)";
+        PreparedStatement ps = cnx.prepareStatement(query);
+        ps.setString(1, reclamation.getTitre());
+        ps.setString(2, reclamation.getDescription());
+        ps.setDate(3, Date.valueOf(reclamation.getDateDemande())); // Attention conversion LocalDate -> SQL Date
+        ps.setString(4, reclamation.getStatut());
+        ps.executeUpdate();
+    }
+
+    @Override
+    public void update(Reclamations reclamation) throws SQLException {
+        String query = "UPDATE reclamations SET titre = ?, description = ?, date_creation = ?, status = ? WHERE id = ?";
+        PreparedStatement ps = cnx.prepareStatement(query);
+        ps.setString(1, reclamation.getTitre());
+        ps.setString(2, reclamation.getDescription());
+        ps.setDate(3, Date.valueOf(reclamation.getDateDemande()));
+        ps.setString(4, reclamation.getStatut());
+        ps.setInt(5, reclamation.getId());
+        ps.executeUpdate();
+    }
+
+    @Override
+    public void delete(Reclamations reclamation) throws SQLException {
+        String query = "DELETE FROM reclamations WHERE id = ?";
+        PreparedStatement ps = cnx.prepareStatement(query);
+        ps.setInt(1, reclamation.getId());
+        ps.executeUpdate();
+    }
+
+    @Override
+    public List<Reclamations> readAll() throws SQLException {
+        List<Reclamations> reclamations = new ArrayList<>();
+        String query = "SELECT * FROM reclamations";
+        Statement st = cnx.createStatement();
+        ResultSet rs = st.executeQuery(query);
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String titre = rs.getString("titre");
+            String description = rs.getString("description");
+            Date dateCreation = rs.getDate("date_creation");
+            String status = rs.getString("status");
+
+            Reclamations reclamation = new Reclamations(id, titre, description, dateCreation.toLocalDate(), status);
+            reclamations.add(reclamation);
+        }
+
+        return reclamations;
+    }
+}
