f7b8c36075c0d7989b466dbe165d668474ea28cc
nouhaabdelli <nouha.abdelli@esprit.tn>
2025-04-27 17:30:25 +0100

    Create main.java

---
+++ Ressourcehumaines/src/main/java/main/main.java
@@ -0,0 +1,24 @@
+package main;
+import java.sql.SQLException;
+
+import entities.Reclamations;
+import services.ReclamationService;
+import java.time.LocalDate;
+
+public class main {
+
+    public static void main(String[] args) {
+        ReclamationService rs = new ReclamationService();
+
+        // Créer une réclamation
+        Reclamations r = new Reclamations(0,"Problème de paie", "Je n'ai pas reçu mon salaire", LocalDate.now(), "En attente");
+
+        try {
+            rs.create(r); // Ajouter une réclamation
+            System.out.println(rs.readAll()); // Lire toutes les réclamations et les afficher
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    }
+
