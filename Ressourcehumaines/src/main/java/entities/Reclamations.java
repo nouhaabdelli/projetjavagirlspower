58b8be64d63f64191075e5247f805e43bc95d517
nouhaabdelli <nouha.abdelli@esprit.tn>
2025-04-29 22:48:54 +0100

    Update Reclamations.java

--- Ressourcehumaines/src/main/java/entities/Reclamations.java
+++ Ressourcehumaines/src/main/java/entities/Reclamations.java
@@ -1,7 +1,7 @@
 package entities;
 
-import java.io.File;
 import java.time.LocalDate;
+import java.io.File;
 
 public class Reclamations {
         private int id; // Identifiant de la réclamation
@@ -10,15 +10,19 @@ public class Reclamations {
         private LocalDate dateDemande; // Date d'envoi de la réclamation
         private String statut; // Statut de la réclamation (en attente, traité, etc.)
         private  String cheminPieceJointe ;
+        private String priorite ;
+        private String RecevoirNotifications ;
 
     // Constructeur
-        public Reclamations(int id, String titre, String description, LocalDate dateDemande, String statut , String cheminPieceJointe) {
+        public Reclamations(int id, String titre, String description, LocalDate dateDemande, String statut , String cheminPieceJointe , String priorite, String RecevoirNotifications) {
             this.id = id;
             this.titre = titre;
             this.description = description;
             this.dateDemande = dateDemande;
             this.statut = statut;
             this.cheminPieceJointe = cheminPieceJointe;
+            this.priorite = priorite;
+            this.RecevoirNotifications = RecevoirNotifications;
         }
 
         // Getters et setters
@@ -37,10 +41,20 @@ public class Reclamations {
         public String getStatut() { return statut; }
         public void setStatut(String statut) { this.statut = statut; }
         public String getCheminPieceJointe() { return cheminPieceJointe; }
-    public void setCheminPieceJointe(String cheminPieceJointe) {
+
+         public void setCheminPieceJointe(String cheminPieceJointe) {
             this.cheminPieceJointe = cheminPieceJointe;
     }
 
+         public String getPriorite() { return priorite; }
+
+         public void setPriorite(String priorite) { this.priorite = priorite; }
+
+         public String RecevoirNotifications() { return RecevoirNotifications; }
+
+         public void setRecevoirNotifications(String recevoirNotifications) {
+        RecevoirNotifications = recevoirNotifications;
     }
+}
 
 
